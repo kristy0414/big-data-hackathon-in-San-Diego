9948b8091430dc7dcc267be787823d0f61955504
discosplicer <jalarocca@sdsu.edu>
2017-02-18 17:27:03 -0800

    Added R script

---
+++ data_analysis/merger.R
@@ -0,0 +1,21 @@
+require(xlsx)
+require(dplyr)
+require(plyr)
+
+mood_dis <- read.csv("data/mood_filtered.csv", header=T)
+mood_dis.edit <- filter(mood_dis, YEAR==2010)
+mood_dis.edit <- filter(mood_dis.edit, UrbanicitySort != 99)
+mood_dis.edit$Geography <- as.character(mood_dis.edit$Geography)
+mood_dis.edit$Geography[mood_dis.edit$Geography == "San Dieguito"] <- "Encinitas"
+mood_dis.edit$Geography[mood_dis.edit$Geography == "Del Mar-Mira Mesa"] <- "Del Mar"
+sd <- data.frame(mood_dis.edit[sapply("San Diego", grepl, mood_dis.edit$Geography),])
+mood_dis.edit <- rbind.fill(mood_dis.edit, data.frame(Geography="San Diego", Age_Adjusted_Rate=mean(sd$Age_Adjusted_Rate)))
+mood_dis <- mood_dis.edit
+
+parks <- read.xlsx("data/park_data.xlsx", 1)
+parks$Geography <- gsub("\\s*\\w*$", "", parks$geoname)
+
+final <- merge(parks, mood_dis, by="Geography")
+
+fit <- lm(Age_Adjusted_Rate ~ p_parkacc, data=final)
+summary(fit)
