36820d5b4ac030619804b1006555f751faf73e83
discosplicer <jalarocca@sdsu.edu>
2017-02-25 09:47:11 -0800

    moved to data folder

--- data_analysis/merger.R
+++ data_analysis/merger.R
@@ -14,7 +14,13 @@ mood_dis.edit <- filter(mood_dis.edit, UrbanicitySort != 99)
 mood_dis.edit$Geography <- as.character(mood_dis.edit$Geography)
 mood_dis.edit$Geography[mood_dis.edit$Geography == "San Dieguito"] <- "Encinitas"
 mood_dis.edit$Geography[mood_dis.edit$Geography == "Del Mar-Mira Mesa"] <- "Del Mar"
+mood_dis.edit$Geography[mood_dis.edit$Geography == "Palomar-Julian"] <- "Julian"
+mood_dis.edit$Geography[mood_dis.edit$Geography == "University Coastal"] <- "La Jolla"
+mood_dis.edit$Geography[mood_dis.edit$Geography == "Anza-Borrego Springs"] <- "Borrego Springs"
+mood_dis.edit$Geography[mood_dis.edit$Geography == "Laguna-Pine Valley"] <- "Pine Valley"
+mood_dis.edit$Geography[mood_dis.edit$Geography == "North San Diego"] <- "Rancho Santa Fe"
 sd <- data.frame(mood_dis.edit[sapply("San Diego", grepl, mood_dis.edit$Geography),])
+sd <- rbind(sd, mood_dis.edit[mood_dis.edit$Geography == "Mid-City",])
 mood_dis.edit <- rbind.fill(mood_dis.edit, data.frame(Geography="San Diego", Age_Adjusted_Rate=mean(sd$Age_Adjusted_Rate)))
 mood_dis <- mood_dis.edit
 
@@ -23,5 +29,5 @@ parks$Geography <- gsub("\\s*\\w*$", "", parks$geoname)
 
 final <- merge(parks, mood_dis, by="Geography")
 
-fit <- lm(Age_Adjusted_Rate ~ p_parkacc, data=final)
-summary(fit)
+fit <- cor(final$Age_Adjusted_Rate, final$p_parkacc)
+fit
