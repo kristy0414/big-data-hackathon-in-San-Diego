5f9c2ae3c597651747dcad055768f70a7c681d83
discosplicer <jalarocca@sdsu.edu>
2017-02-25 13:03:01 -0800

    added mood_parks.csv

--- data_analysis/filter.R
+++ data_analysis/filter.R
@@ -24,12 +24,12 @@ sd <- rbind(sd, mood_dis.edit[mood_dis.edit$Geography == "Mid-City",])
 mood_dis.edit <- rbind.fill(mood_dis.edit, data.frame(Geography="San Diego", Age_Adjusted_Rate=mean(sd$Age_Adjusted_Rate)))
 #write.csv(mood_dis.edit, "js/data/mood_disorders.csv")
 
-parks <- read.xlsx("data/HCI_PovertyRate_754_CT_PL_CO_RE_CA_1-22-14.xlsx", 1)
+parks <- read.xlsx("data/HCI_Crime_752_PL_CO_RE_CA_2000-2013_21OCT15.xlsx", 1)
 parks <- parks[parks$race_eth_code == 9 & parks$geotype == "PL" & !is.na(parks$rate) & parks$county_name=="San Diego",]
 parks$Geography <- gsub("\\s*\\w*$", "", parks$geoname)
 #write.csv(parks, "js/data/parks_filtered.csv")
-final <- merge(parks, mood_dis, by="Geography")
+final <- merge(parks, mood_dis.edit, by="Geography")
 
 fit <- cor(final$Age_Adjusted_Rate, final$rate)
 fit
-#write.csv(final, "data_analysis/mood_poverty.csv")
+write.csv(final, "data_analysis/mood_crime.csv")
