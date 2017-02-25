cdc8c2e893e9a021299a6b2a184b6864f47ee318
discosplicer <jalarocca@sdsu.edu>
2017-02-25 15:54:49 -0800

    added new datasets in data-analysis

--- data_analysis/filter2.R
+++ data_analysis/filter2.R
@@ -10,17 +10,17 @@ mood_dis.edit <- mood_dis[mood_dis$YEAR == 2010, ]
 mood_dis.edit <- mood_dis.edit[mood_dis.edit$UrbanicitySort != 99, ]
 mood_dis.edit$Geography <- toupper(as.character(mood_dis.edit$Geography))
 
-parks <- read.xlsx("data/HCI_Unemployment_290_CA_RE_CO_CD_PL_CT-5-22-14.xlsx", 1)
-parks <- parks[parks$race_eth_code == 9 & parks$geotype == "CT" & !is.na(parks$Unemployment_rate) & parks$county_name=="San Diego",]
+parks <- read.xlsx("data/ParkBeachOpen10_output4-12-13.xlsx", 1)
+parks <- parks[parks$race_eth_code == 9 & parks$geotype == "CT" & !is.na(parks$p_parkacc) & parks$county_name=="San Diego",]
 #parks$TRACT <- as.numeric(lapply(strsplit(parks$geoname, " "), '[', 3))
 parks$TRACT <- as.numeric(parks$geoname)
 
 regions <- read.xlsx("data/assist_tables/census_subregion.xlsx", 1)
-parks$region <- merge(parks, regions, all.x=T)[,"NAME"]
-ppark <- aggregate(parks$Unemployment_rate, list(parks$region), mean)
-colnames(ppark) <- c("Geography", "UnemploymentRate")
-final <- merge(ppark, mood_dis.edit)
+parks$Geography <- merge(parks, regions, all.x=T)[,"NAME"]
+#ppark <- aggregate(parks$Unemployment_rate, list(parks$region), mean)
+#colnames(ppark) <- c("Geography", "UnemploymentRate")
+final <- merge(parks, mood_dis.edit)
 
-fit <- cor(final$Age_Adjusted_Rate, final$UnemploymentRate)
+fit <- cor(final$Age_Adjusted_Rate, final$Unemployment_rate)
 fit
-write.csv(final, "data_analysis/mood_unemployment.csv")
+write.csv(final, "data_analysis/mood_unemployment2.csv")
