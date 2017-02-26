83bd401696c3994e65686df8714988b6ae35a3de
discosplicer <jalarocca@sdsu.edu>
2017-02-26 08:53:04 -0800

    added rpart data

--- data_analysis/filter2.R
+++ data_analysis/filter2.R
@@ -10,8 +10,8 @@ mood_dis.edit <- mood_dis[mood_dis$YEAR == 2010, ]
 mood_dis.edit <- mood_dis.edit[mood_dis.edit$UrbanicitySort != 99, ]
 mood_dis.edit$Geography <- toupper(as.character(mood_dis.edit$Geography))
 
-parks <- read.xlsx("data/ParkBeachOpen10_output4-12-13.xlsx", 1)
-parks <- parks[parks$race_eth_code == 9 & parks$geotype == "CT" & !is.na(parks$p_parkacc) & parks$county_name=="San Diego",]
+parks <- read.xlsx("data/HCI_PovertyRate_754_CT_PL_CO_RE_CA_1-22-14.xlsx", 1)
+parks <- parks[parks$race_eth_code == 9 & parks$geotype == "CT" & !is.na(parks$percent) & parks$county_name=="San Diego",]
 #parks$TRACT <- as.numeric(lapply(strsplit(parks$geoname, " "), '[', 3))
 parks$TRACT <- as.numeric(parks$geoname)
 
@@ -21,6 +21,6 @@ parks$Geography <- merge(parks, regions, all.x=T)[,"NAME"]
 #colnames(ppark) <- c("Geography", "UnemploymentRate")
 final <- merge(parks, mood_dis.edit)
 
-fit <- cor(final$Age_Adjusted_Rate, final$Unemployment_rate)
+fit <- cor(final$Age_Adjusted_Rate, final$percent)
 fit
-write.csv(final, "data_analysis/mood_unemployment2.csv")
+write.csv(final, "data_analysis/mood_poverty2.csv")
